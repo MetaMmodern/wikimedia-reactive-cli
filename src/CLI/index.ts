5387512c35430ab9f81a8e7d83d50b1c84a0dacb
metamodern <shabanovbunya@gmail.com>
2022-01-06 22:16:40 +0200

    add better code support for rx event and add state indicator

--- src/CLI/index.ts
+++ src/CLI/index.ts
@@ -1,12 +1,14 @@
+import chalk from "chalk";
 import { drawChart } from "./../types/chats/index";
-import { scan, throttle } from "rxjs/operators";
+import { map, scan, throttle } from "rxjs/operators";
 import UpdateTypesStatistics from "../wikiUpdateTypes/UpdateTypesStatistics";
 import MostActiveUserStatistics from "../wikiMostActiveUser/MostActiveUserStatistics";
 import { MostActiveUserStatisticsType } from "../wikiMostActiveUser/MostActiveUserStatisticsType";
 import { interval, Observable, Subscription } from "rxjs";
 import "../wikiSubscriber";
 
-const GetUpdateStatisticsSubscription = () => {
+// 4 messages edit new log categorize (text/mode)
+const UpdateStatisticsObservable = () => {
   return UpdateTypesStatistics.pipe(
     scan(
       (acc, cur) => {
@@ -27,9 +29,9 @@ const GetUpdateStatisticsSubscription = () => {
     throttle(() => interval(500))
     // concatMap((item) => of(item).pipe(delay(100)))
   );
-}
+};
 
-const GetUpdateStatisticsSubscriber = (logFunction: CallableFunction) => {
+const UpdateStatisticsGraphicSubscriber = (logFunction: Function) => {
   return (data: {
     new: number[];
     log: number[];
@@ -45,27 +47,36 @@ const GetUpdateStatisticsSubscriber = (logFunction: CallableFunction) => {
         colorsNames: ["yellow", "magenta", "blue", "red"],
       })
     );
-  }
-}
+  };
+};
+const UpdateStatisticsTextSubscriber = (logFunction: Function) => {
+  return (data: {
+    new: number;
+    log: number;
+    edit: number;
+    categorize: number;
+  }) => {
+    console.clear();
+    process.stdout.write("\n");
 
+    logFunction(chalk.bgGreen(JSON.stringify(data, null, 2)));
+  };
+};
+
+// (list: time, user, contributions)
 const GetMostActiveUserSubscription = () => {
-  return MostActiveUserStatistics.pipe(
-    throttle(() => interval(10000))
-  );
-}
+  return MostActiveUserStatistics.pipe(throttle(() => interval(10000)));
+};
 
-const GetMostActiveUserSubscriber = (logFunction: CallableFunction) => {
+const GetMostActiveUserSubscriber = (logFunction: Function) => {
   return (data: MostActiveUserStatisticsType) => {
     console.clear();
     process.stdout.write("\n");
 
-    logFunction(
+    logFunction();
+  };
+};
 
-    );
-  }
-}
-
-declare type subscriberFunctionType = (logFunction: CallableFunction) => (data: any) => void
 class CLIConsumer {
   readonly baseActions = {
     "[u]": "users",
@@ -83,6 +94,11 @@ class CLIConsumer {
     "[2]": "30 seconds",
     "[3]": "1 minute",
   };
+  readonly stateSelectionActions = {
+    "[1]": "10 seconds",
+    "[2]": "30 seconds",
+    "[3]": "1 minute",
+  };
   currentOperation:
     | "user_selection"
     | "interval_selection"
@@ -91,50 +107,104 @@ class CLIConsumer {
     | "" = "";
   currentInterval: "1m" | "30s" | "10s" = "10s";
   currentMode: "text" | "graph" = "graph";
+  currentState:
+    | "most_active_users"
+    | "most_typoed_articles"
+    | "edit_types_statistics" = "edit_types_statistics";
   usersList: string[] = [];
   showsStats: boolean = false;
   statsType: string = "";
   currentObservable:
     | Observable<{
-      new: number[];
-      log: number[];
-      edit: number[];
-      categorize: number[];
-    }>
-    | Observable<MostActiveUserStatisticsType>
-    | undefined = undefined;
-  currentSubscription: Subscription | undefined = undefined;
+        new: number[];
+        log: number[];
+        edit: number[];
+        categorize: number[];
+      }>
+    | Observable<MostActiveUserStatisticsType>;
+  currentSubscription: Subscription;
   constructor() {
     this.logWithUserPrompt("");
     process.stdin.setRawMode(true);
     process.stdin.addListener("data", this.rootStdInListener.bind(this));
 
-    this.currentObservable = GetUpdateStatisticsSubscription();
+    this.currentObservable = UpdateStatisticsObservable();
     this.currentSubscription = this.currentObservable.subscribe(
-      this.defaultSubscriber.bind(this)
+      this.currentSubscriber.bind(this)
     );
   }
 
-  defaultSubscriber: subscriberFunctionType = GetUpdateStatisticsSubscriber(this.logWithUserPrompt);
+  rxResolver() {
+    if (this.currentOperation !== "") {
+      this.currentSubscription.unsubscribe();
+      return;
+    }
+    switch (this.currentState) {
+      case "edit_types_statistics":
+        if (this.currentMode === "graph") {
+          this.currentObservable = UpdateStatisticsObservable();
+          this.currentSubscription = this.currentObservable?.subscribe(
+            UpdateStatisticsGraphicSubscriber(
+              this.logWithUserPrompt.bind(this)
+            ).bind(this)
+          );
+        } else {
+          this.currentObservable = UpdateStatisticsObservable();
+          this.currentSubscription = this.currentObservable
+            .pipe(
+              map(
+                (v) =>
+                  Object.fromEntries(
+                    Object.entries(v).map((e) => [
+                      e[0],
+                      e[1].reduce((acc, curr) => acc + curr, 0),
+                    ])
+                  ) as {
+                    new: number;
+                    log: number;
+                    edit: number;
+                    categorize: number;
+                  }
+              )
+            )
+            .subscribe(
+              UpdateStatisticsTextSubscriber(this.logWithUserPrompt.bind(this))
+            );
+        }
+        break;
+      case "most_active_users":
+        break;
+      case "most_typoed_articles":
+        break;
+      default:
+        break;
+    }
+  }
+
+  currentSubscriber: Function = UpdateStatisticsGraphicSubscriber(
+    this.logWithUserPrompt.bind(this)
+  );
 
   actionsListener(data: Buffer) {
-    this.currentSubscription?.unsubscribe();
     const userInput = data.toString()[0];
     switch (userInput) {
       case "u":
         this.promptUsersList();
         break;
       case "i":
-        this.logWithUserPrompt("ahaha\n");
+        this.promptIntervalList();
         break;
       case "m":
         this.promptModeList();
         break;
       case "s":
-        this.logWithUserPrompt("ssssssssss\n");
+        this.currentSubscription?.unsubscribe();
+        this.promptState();
         break;
+      case "q":
+        process.exit(0);
       default:
-        this.logWithUserPrompt("");
+        // this.logWithUserPrompt("");
         break;
     }
   }
@@ -143,18 +213,36 @@ class CLIConsumer {
     process.stdin.setRawMode(false);
     console.clear();
     this.currentOperation = "user_selection";
+    this.rxResolver();
     process.stdout.write("Enter coma separated users filter list: ");
   }
 
   promptModeList() {
     console.clear();
     this.currentOperation = "mode_selection";
+    this.rxResolver();
     process.stdout.write(
       Object.entries(this.modeSelectionActions)
         .map((entry) => entry[0] + " " + entry[1])
         .join("\n")
     );
     process.stdout.write("\nSelect number for a mode: ");
+  }
+
+  promptIntervalList() {
+    console.clear();
+    this.currentOperation = "interval_selection";
+    this.rxResolver();
+    process.stdout.write(
+      Object.entries(this.intervalSelectionActions)
+        .map((entry) => entry[0] + " " + entry[1])
+        .join("\n")
+    );
+    process.stdout.write("\nSelect number for an interval: ");
+  }
+  promptState() {
+    this.currentOperation = "stats_selection";
+    this.rxResolver();
   }
 
   rootStdInListener(data: Buffer) {
@@ -192,6 +280,7 @@ class CLIConsumer {
       this.currentOperation = "";
       this.currentMode = selection === "1" ? "text" : "graph";
       this.rootStdInListener(Buffer.from(""));
+      this.rxResolver();
       return;
     }
     console.clear();
@@ -199,21 +288,23 @@ class CLIConsumer {
   }
 
   intervalSelected(data: Buffer) {
-    process.stdin.setRawMode(true);
-
     const selection = data.toString()[0];
     if (selection == "1" || selection == "2" || selection == "3") {
       this.currentOperation = "";
-      this.currentInterval = selection === "1" ? "1m" : selection === "2" ? "30s" : "10s";
+      this.currentInterval =
+        selection === "1" ? "1m" : selection === "2" ? "30s" : "10s";
       this.rootStdInListener(Buffer.from(""));
+      this.currentObservable = GetMostActiveUserSubscription();
+      this.currentSubscriber = GetMostActiveUserSubscriber(
+        this.logWithUserPrompt.bind(this)
+      );
+      this.currentSubscription = this.currentObservable?.subscribe(
+        this.currentSubscriber.bind(this)
+      );
       return;
     }
-
-    this.currentObservable = GetMostActiveUserSubscription();
-    this.defaultSubscriber = GetMostActiveUserSubscriber(this.logWithUserPrompt);
-    this.currentSubscription = this.currentObservable?.subscribe(
-      this.defaultSubscriber.bind(this)
-    );
+    // console.clear();
+    else this.promptIntervalList();
   }
 
   usersSelected(data: Buffer) {
@@ -225,14 +316,8 @@ class CLIConsumer {
       .split(",")
       .map((s) => s.trim());
     this.rootStdInListener(Buffer.from(""));
-
-    this.currentObservable = GetUpdateStatisticsSubscription();
-    this.defaultSubscriber = GetUpdateStatisticsSubscriber(this.logWithUserPrompt);
-    this.currentSubscription = this.currentObservable?.subscribe(
-      this.defaultSubscriber.bind(this)
-    );
+    this.rxResolver();
   }
-
 }
 
 const instance = new CLIConsumer();
