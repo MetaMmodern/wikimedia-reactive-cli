205a21feadb25ae208089a92f44f1c3eb4ad77b4
Nikita <nikita.molodtsov21@gmail.com>
2022-01-07 01:17:46 +0200

    Merged new cli

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 import "./CLI";
-// import "./types/chats/index";
-
-// import "./wikiSubscriber";
-// import "./CLI";
-// import "./wikiTypoedArticles"
-// import "./wikiMostActiveUser/MostActiveUserStatistics"
