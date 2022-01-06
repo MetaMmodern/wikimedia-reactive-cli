141de125b8c4fd875ce7e487b5d350ad7894fe59
Nikita <nikita.molodtsov21@gmail.com>
2022-01-06 06:49:36 +0200

    merged third task

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 // import "./wikiSubscriber";
 // import "./CLI";
 // import "./wikiTypoedArticles"
-<<<<<<< HEAD
 import "./wikiMostActiveUser/MostActiveUserStatistics"
-=======
-import "./wikiContributsAmountPerTime"
->>>>>>> remotes/origin/OlexandrBebeh/Actions
